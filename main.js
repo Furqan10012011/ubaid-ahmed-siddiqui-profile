7e415f97a13ed6cb72ea9b0aae95e2df4aba2194
Furqan Siddiqui <furqan.siddiqui101@gmail.com>
2026-06-15 23:28:17 +0530

    Refactor loader and navbar functionality

--- main.js
+++ main.js
@@ -94,3 +94,11 @@ if(menuToggle){
     });
 
 }
+
+
+const menu = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav-links");
+
+menu.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
